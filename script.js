572fbddefef6b826f3ad39f7f500364165539b6b
Chan Myae Kyaw <chanmyaekyaw@Chans-MacBook-Air.local>
2025-12-15 23:17:54 +0630

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Existing functions
+function goToPage1() { window.location.href = "page1.html"; }
+
+function goToPage2() { window.location.href = "page2.html"; }
+
+function goToPage3() { window.location.href = "page3.html"; }
+
+// New functions for Page 3 cards
+function goToGallery() { window.location.href = "gallery.html"; }
+
+function goToBouquet() { window.location.href = "bouquet.html"; }
+
+function goToLetter() { window.location.href = "loveletter.html"; }
+
+function goToSong() { window.location.href = "song.html"; }
+
+function goToWish() { window.location.href = "wish.html"; }
+
+function goToCandles() { window.location.href = "candles.html"; }
+
+function goBack() { window.location.href = "page1.html"; }
