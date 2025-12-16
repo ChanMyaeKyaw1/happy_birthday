a5a58d54cccb15b2cb4f0016fb8805bd8bdafa86
Chan Myae Kyaw <chanmyaekyaw@Chans-MacBook-Air.local>
2025-12-16 15:38:39 +0630

    Fix HTML structure: move img and correct paths

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ function goToWish() { window.location.href = "wish.html"; }
 function goToCandles() { window.location.href = "candles.html"; }
 
 function goBack() { window.location.href = "index.html"; }
+
+function goToGiftIntro() { window.location.href = "gift_intro.html"; }
+
+function goToGift() { window.location.href = "gift.html"; }
+
+function getHint() {
+    Swal.fire({
+        title: 'Hint!',
+        text: 'Look around the place where you usually relax!',
+        icon: 'info',
+        confirmButtonText: 'Got it!'
+    });
+    // window.location.href = "envelope.html"; // Uncomment if you want to go to next page
+}
+
+function takePhoto() { window.location.href = "takePhoto.html"; }
+
+// function takePhoto() { window.location.href = "https://photobooth.nashallery.com/index.html"; }
+
+function goToRating() { window.location.href = "rating.html"; }
